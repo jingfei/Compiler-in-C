25e6c8011b07d8b413ec7d581ac9a0930db3a496
Jingfei <jingfei955047@gmail.com>
2015-06-19 17:10:31 +0800

    SymbolTable .text can output simple logic

--- main.c
+++ main.c
@@ -1,22 +1,12 @@
 int x ;
 
-int plus ( int a , int b ) {
-	int c ;
-	c = a + b ;
-	return 0 ;
-}
-
 int main (  ) {
-	int y [ 2 ] ;
-	y [ 0 ]  = 3 ;
-	y [ 1 ]  = 4 ;
-	x = plus( y [ 0 ] , y [ 1 ] ) ;
-	if ( x ) {
-		double a;
-	}
-	else {
-		float b ;
-	}
-//	print x ;
-//	printf("%d",x);
+	int a;
+	int b;
+	int c;
+	a = 0;
+	b = 1;
+	c = 2;
+	x = a + b -c;
+	print x;
 }
---
+++ main_v3.c
@@ -0,0 +1,21 @@
+int x ;
+
+int plus ( int a , int b ) {
+	int c ;
+	c = a + b ;
+	return 0 ;
+}
+
+int main (  ) {
+	int y [ 2 ] ;
+	y [ 0 ]  = 3 ;
+	y [ 1 ]  = 4 ;
+	x = plus( y [ 0 ] , y [ 1 ] ) ;
+	if ( x ) {
+		double a;
+	}
+	else {
+		float b ;
+	}
+	print x ;
+}
